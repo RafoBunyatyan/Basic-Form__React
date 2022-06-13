3d55ff69fe3c645e8ac43c9df67e837b7d42c9fe
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-06-13 17:51:35 +0400

    add useRef & useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,46 @@
+import React, { useRef, useState } from 'react'
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const nameInputRef = useRef()
+	const [enteredName, setEnteredName] = useState('')
+
+	const nameInputChangeHandler = event => {
+		setEnteredName(event.target.value)
+	}
+
+	const formSubmitionHandler = event => {
+		event.preventDefault()
+
+		if (enteredName.trim() === '') {
+			return;
+		}
+
+		console.log(enteredName);
+
+		const enteredValue = nameInputRef.current.value
+		console.log(enteredValue);
+
+		// nameInputRef.current.value = '' // NOT IDEAL, DON`T MANIPULATE THE DOM
+		setEnteredName('')
+	}
+
+	return (
+		<form onSubmit={formSubmitionHandler}>
+			<div className='form-control'>
+				<label htmlFor='name'>Your Name</label>
+				<input
+					ref={nameInputRef}
+					type='text'
+					id='name'
+					onChange={nameInputChangeHandler}
+					value={enteredName}
+				/>
+			</div>
+			<div className="form-actions">
+				<button>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default SimpleInput;
